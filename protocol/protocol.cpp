aa35392071ab202908d551b0c82ab3f3d3746a21
shogoshima <shogoshima@usp.br>
2024-12-03 13:18:03 -0300

    protocol integration in makefile

--- protocol/protocol.cpp
+++ protocol/protocol.cpp
@@ -40,56 +40,56 @@ ClientMessage ClientMessage::decode(const std::string &data) {
 }
 
 // test script
-int main() {
-    std::string encoded;
+// int main() {
+//     std::string encoded;
 
-    // Client Message test script
-    std::cout << "Client message test:" << std::endl;
-    ClientMessage client(1, 300);
+//     // Client Message test script
+//     std::cout << "Client message test:" << std::endl;
+//     ClientMessage client(1, 300);
 
-    // Client that was created
-    std::cout << "ID: " << client.data.id
-              << ", Score: " << client.data.score
-              << ", Timestamp: " << client.data.timestamp << std::endl;
+//     // Client that was created
+//     std::cout << "ID: " << client.data.id
+//               << ", Score: " << client.data.score
+//               << ", Timestamp: " << client.data.timestamp << std::endl;
 
-    // Encoding message to send to server
-    encoded = client.encode();
-    std::cout << "Encoded: " << encoded << std::endl;
+//     // Encoding message to send to server
+//     encoded = client.encode();
+//     std::cout << "Encoded: " << encoded << std::endl;
 
-    // Decoding message sent from client
-    ClientMessage decodedClient = ClientMessage::decode(encoded);
-    std::cout << "Decoded ID: " << decodedClient.data.id
-              << ", Score: " << decodedClient.data.score
-              << ", Timestamp: " << decodedClient.data.timestamp << std::endl;
+//     // Decoding message sent from client
+//     ClientMessage decodedClient = ClientMessage::decode(encoded);
+//     std::cout << "Decoded ID: " << decodedClient.data.id
+//               << ", Score: " << decodedClient.data.score
+//               << ", Timestamp: " << decodedClient.data.timestamp << std::endl;
 
-    // Server Message test script
-    std::cout << "\nServer message test:" << std::endl;
-    ServerMessage server(3);
+//     // Server Message test script
+//     std::cout << "\nServer message test:" << std::endl;
+//     ServerMessage server(3);
 
-    // Adding players
-    // I order the players by score (descending), and then by timestamp (ascending)
-    server.insert(1, 100, 12);
-    server.insert(2, 200, 13);
-    server.insert(3, 200, 14);
+//     // Adding players
+//     // I order the players by score (descending), and then by timestamp (ascending)
+//     server.insert(1, 100, 12);
+//     server.insert(2, 200, 13);
+//     server.insert(3, 200, 14);
 
-    // Display players in descending order by score
-    for (const auto &player : server.allData) {
-        std::cout << "ID: " << player.id
-                  << ", Score: " << player.score
-                  << ", Timestamp: " << player.timestamp << std::endl;
-    }
+//     // Display players in descending order by score
+//     for (const auto &player : server.allData) {
+//         std::cout << "ID: " << player.id
+//                   << ", Score: " << player.score
+//                   << ", Timestamp: " << player.timestamp << std::endl;
+//     }
 
-    // Encoding message to send to client
-    encoded = server.encode();
-    std::cout << "Encoded " << encoded << std::endl;
+//     // Encoding message to send to client
+//     encoded = server.encode();
+//     std::cout << "Encoded " << encoded << std::endl;
 
-    // Decoding message sent from server
-    ServerMessage decodedServer = ServerMessage::decode(encoded);
-    std::cout << "Player Count: " << decodedServer.playerCount << std::endl;
-    for (const auto &player : decodedServer.allData) {
-        std::cout << "Decoded ID: " << player.id
-                  << ", Score: " << player.score << std::endl;
-    }
+//     // Decoding message sent from server
+//     ServerMessage decodedServer = ServerMessage::decode(encoded);
+//     std::cout << "Player Count: " << decodedServer.playerCount << std::endl;
+//     for (const auto &player : decodedServer.allData) {
+//         std::cout << "Decoded ID: " << player.id
+//                   << ", Score: " << player.score << std::endl;
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
