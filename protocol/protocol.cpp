253b84aff7237252b259268a028b1b13d4eeee5f
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2024-12-10 17:35:15 -0300

    protocolo de comunicacao completo

--- protocol/protocol.cpp
+++ protocol/protocol.cpp
@@ -2,96 +2,142 @@
 
 std::string ServerMessage::encode() {
     std::ostringstream oss;
-    oss << playerCount << " " << isRunning << " ";
-    for (const auto &data : rankings) {
-        oss << data.second.first << " " << data.second.second << " " << data.first << " ";
+    oss << int(type) << " ";
+    switch (type) {
+    case ServerMessageType::START :
+        oss << playerCount;
+        break;
+    
+    case ServerMessageType::RANKING :
+        oss << playerCount << " " << isRunning << " ";
+        for (const auto &data : rankings) {
+            oss << data.second.first << " " << data.second.second << " " << data.first << " ";
+        } 
+        break;
+    
+    case ServerMessageType::PHRASE :
+        oss << playerCount << " " << phrase;
+        break;
+
+    case ServerMessageType::END_GAME :
+        oss << playerCount << " ";
+        for (const auto &data : rankings) {
+            oss << data.second.first << " " << data.second.second << " " << data.first << " ";
+        }
+        break;
+
+    default:
+        perror("ERROR ServerMessageType not reconigzed");
+        break;
     }
+
     return oss.str();
 }
 
 ServerMessage ServerMessage::decode(const std::string &data) {
     std::istringstream iss(data);
+    ServerMessageType in_type;
+    int temp1, temp2;
+    std:: string temps;
+    iss >> temp1;
+    in_type = ServerMessageType(temp1);
     int count;
-    iss >> count;
-    ServerMessage message(count);
+    ServerMessage message;
+    message.type = in_type;
 
-    iss >> message.isRunning;
-    while (!iss.eof()) {
-        std::string name;
-        int score, timestamp;
-        if (iss >> score >> timestamp >> name) {
-            message.rankings.insert({name, {score, timestamp}});
+    switch (in_type) {
+    case ServerMessageType::START :
+        iss >> message.playerCount;
+        break;
+    
+    case ServerMessageType::RANKING :
+        iss >> message.playerCount >> message.isRunning;
+        for (int i = 0; i < message.playerCount; i++){
+            // Reads the ranking, following the conventions in encode
+            iss >> temp1 >> temp2 >> temps;
+            message.rankings.insert({temps, {temp1, temp2}});
         }
+        break;
+    
+    case ServerMessageType::PHRASE :
+        iss >> message.playerCount >> message.phrase;
+        break;
+
+    case ServerMessageType::END_GAME :
+        iss >> message.playerCount;
+        for (int i = 0; i < message.playerCount; i++){
+            // Reads the ranking, following the conventions in encode
+            iss >> temp1 >> temp2 >> temps;
+            message.rankings.insert({temps, {temp1, temp2}});
+        }
+        break;
+
+    default:
+        perror("ERROR ServerMessageType not reconigzed");
+        break;
     }
+    
+    
     return message;
 }
 
 // client message encoding and decoding
 std::string ClientMessage::encode() {
     std::ostringstream oss;
-    oss << data.name << " " << data.score << " " << data.timestamp;
+    oss << int(type) << " ";
+    switch (type)
+    {
+    case ClientMessageType::READY :
+        oss << data.name;
+        break;
+    
+    case ClientMessageType::PROGRESS :
+        oss << data.name << " " << data.score << " " << data.timestamp;    
+        break;
+
+    case ClientMessageType::DONE :
+        oss << data.timestamp;
+        break;
+    
+    
+    default:
+        perror("ERROR ClientMessageType not reconigzed");
+        break;
+    }
+    
     return oss.str();
 }
 
 ClientMessage ClientMessage::decode(const std::string &data) {
     std::istringstream iss(data);
-    std::string name;
-    int score, timestamp;
-    iss >> name >> score >> timestamp;
-    return ClientMessage(name, score, timestamp);
+    ClientMessageType in_type;
+    int temp1, temp2;
+    std:: string temps;
+    iss >> temp1;
+    in_type = ClientMessageType(temp1);
+    int count;
+    ClientMessage message(in_type);
+     switch (in_type)
+    {
+    case ClientMessageType::READY :
+        iss >> message.data.name;
+        break;
+    
+    case ClientMessageType::PROGRESS :
+        iss >> message.data.name >> message.data.score >> message.data.timestamp;
+        break;
+
+    case ClientMessageType::DONE :
+        iss >> message.data.timestamp;
+        break;
+    
+    
+    default:
+        perror("ERROR ClientMessageType not reconigzed");
+        break;
+    }
+    
+    
+    return message;
+    
 }
-
-// test script
-// int main() {
-//     std::string encoded;
-
-//     // Client Message test script
-//     std::cout << "Client message test:" << std::endl;
-//     ClientMessage client(1, 300);
-
-//     // Client that was created
-//     std::cout << "ID: " << client.data.id
-//               << ", Score: " << client.data.score
-//               << ", Timestamp: " << client.data.timestamp << std::endl;
-
-//     // Encoding message to send to server
-//     encoded = client.encode();
-//     std::cout << "Encoded: " << encoded << std::endl;
-
-//     // Decoding message sent from client
-//     ClientMessage decodedClient = ClientMessage::decode(encoded);
-//     std::cout << "Decoded ID: " << decodedClient.data.id
-//               << ", Score: " << decodedClient.data.score
-//               << ", Timestamp: " << decodedClient.data.timestamp << std::endl;
-
-//     // Server Message test script
-//     std::cout << "\nServer message test:" << std::endl;
-//     ServerMessage server(3);
-
-//     // Adding players
-//     // I order the players by score (descending), and then by timestamp (ascending)
-//     server.insert(1, 100, 12);
-//     server.insert(2, 200, 13);
-//     server.insert(3, 200, 14);
-
-//     // Display players in descending order by score
-//     for (const auto &player : server.allData) {
-//         std::cout << "ID: " << player.id
-//                   << ", Score: " << player.score
-//                   << ", Timestamp: " << player.timestamp << std::endl;
-//     }
-
-//     // Encoding message to send to client
-//     encoded = server.encode();
-//     std::cout << "Encoded " << encoded << std::endl;
-
-//     // Decoding message sent from server
-//     ServerMessage decodedServer = ServerMessage::decode(encoded);
-//     std::cout << "Player Count: " << decodedServer.playerCount << std::endl;
-//     for (const auto &player : decodedServer.allData) {
-//         std::cout << "Decoded ID: " << player.id
-//                   << ", Score: " << player.score << std::endl;
-//     }
-
-//     return 0;
-// }
